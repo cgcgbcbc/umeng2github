7fbb5f7d44b18fefd31a371fea9c165c3bca6d72
cgcgbcbc <cgcgbcbc@163.com>
2015-07-03 00:23:42 +0800

    add format.go

--- format.go
+++ format.go
@@ -1,1 +1,31 @@
-package umeng2github
+package main
+import (
+	"text/template"
+	"bytes"
+)
+
+const DefaultBodyTemplate = `
+发生次数 | 首次发生时间 | 版本
+-----|-----|-----
+{{.Count}} | {{.FirstDate.Format "2006-01-02 03:04:05"}} | {{.Version}}
+
+StackTrace:
+` +
+"```\n{{.StackTrace}}\n```"
+
+func FormatRecord(record Record) (result string, err error) {
+	tmpl, err := template.New("").Parse(DefaultBodyTemplate)
+	if err != nil {
+		return
+	}
+
+	var b bytes.Buffer
+	err = tmpl.Execute(&b, record)
+	if err != nil {
+		return
+	}
+
+	result = b.String()
+
+	return
+}
--- format_test.go
+++ format_test.go
@@ -1,1 +1,37 @@
-package umeng2github
+package main
+import (
+	"time"
+	"fmt"
+)
+
+func ExampleFormatRecord() {
+	testDate, err := time.Parse("2006-01-02 03:04:05", "2015-06-10 09:21:43")
+	if err != nil {
+		fmt.Print(err)
+		return
+	}
+	record := Record{
+		Title: "test",
+		Count: 2,
+		FirstDate: testDate,
+		Version: "0.1.0",
+		StackTrace: "java.lang.NullPointerException",
+	}
+
+	result, err := FormatRecord(record)
+	if err != nil {
+		fmt.Print(err)
+		return
+	}
+	fmt.Print(result)
+
+	// Output:
+	// 发生次数 | 首次发生时间 | 版本
+	// -----|-----|-----
+	// 2 | 2015-06-10 09:21:43 | 0.1.0
+	//
+	// StackTrace:
+	// ```
+	// java.lang.NullPointerException
+	// ```
+}
